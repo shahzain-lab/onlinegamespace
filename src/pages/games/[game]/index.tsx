e9cbc515fdf87fea0e91c295d29aecd840ed1aa2
shahzain-lab <shahzain30196@gmail.com>
2023-01-22 15:55:59 +0500

    fix([game]): removed try/catch getStaticPaths

--- src/pages/games/[game]/index.tsx
+++ src/pages/games/[game]/index.tsx
@@ -51,7 +51,7 @@ export async function getStaticPaths() {
       url: `${FTP_BASE_URL}/api/games`
     })
     const paths = getGames?.data?.map((game: IGamesList) => {
-      return {params: { game: game.id }}
+      return {params: { game: game.id.toString() }}
     });
 
     return { paths, fallback: false }
